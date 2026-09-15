3dc306ab478399e9ccdab0f2acc32d5b45142ed4
Bimigijoo <bimigijo1212@gmail.com>
2026-09-15 11:24:26 +0530

    Add Spring Boot backend foundation

---
+++ healthbridge-backend/src/main/java/com/healthbridge/healthbridgebackend/HealthbridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.healthbridge.healthbridgebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthbridgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthbridgeBackendApplication.class, args);
+	}
+
+}
---
+++ healthbridge-backend/src/test/java/com/healthbridge/healthbridgebackend/HealthbridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthbridge.healthbridgebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthbridgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
